c5a370ad78488a4c6fe8615a976c32ac35d1e509
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2025-01-07 20:13:20 +0300

    learning/flows - task with fetching + exception task

---
+++ app/src/main/java/com/alenniboris/nba_app/fetched.kt
@@ -0,0 +1,10 @@
+package com.alenniboris.nba_app
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
+
