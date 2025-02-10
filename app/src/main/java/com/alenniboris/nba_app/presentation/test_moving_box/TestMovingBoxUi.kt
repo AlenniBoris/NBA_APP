7485ab6a319be05372f3f748a5da1d5496140a16
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2025-02-10 17:06:26 +0300

    learning/moving_box task 2nd version

--- app/src/main/java/com/alenniboris/nba_app/presentation/test_moving_box/TestMovingBoxUi.kt
+++ app/src/main/java/com/alenniboris/nba_app/presentation/test_moving_box/TestMovingBoxUi.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,9 +16,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
@@ -29,37 +34,69 @@ fun TestMovingBoxUi() {
             .padding(vertical = 100.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val parentBoxSize = 200.dp
-        val childBoxSize = 30.dp
+
+        var parentSize by remember { mutableStateOf(IntSize.Zero) }
+        var parentCoords by remember { mutableStateOf(IntOffset.Zero) }
 
         Box(
             modifier = Modifier
-                .size(parentBoxSize)
-                .background(Color.Black),
+                .width(200.dp)
+                .height(250.dp)
+                .background(Color.Black)
+                .onGloballyPositioned { position ->
+                    parentSize = position.size
+                    parentCoords = position
+                        .positionInRoot()
+                        .toIntOffset()
+                },
             contentAlignment = Alignment.Center
         ) {
 
-            var offsetX by remember { mutableStateOf(0f) }
-            var offsetY by remember { mutableStateOf(0f) }
+            var childSize by remember { mutableStateOf(IntSize.Zero) }
+            var childCoords by remember { mutableStateOf(IntOffset.Zero) }
+
+            var offset by remember { mutableStateOf(IntOffset.Zero) }
 
             Box(
                 modifier = Modifier
-                    .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
-                    .size(childBoxSize)
-                    .background(Color.Red)
-                    .pointerInput(Unit) {
-                        detectDragGestures { change, dragAmount ->
-                            change.consume()
-
-                            val maxOffset = ((parentBoxSize - childBoxSize) / 2).toPx()
-
-                            offsetX =
-                                (offsetX + dragAmount.x).coerceIn(-maxOffset, maxOffset)
-                            offsetY =
-                                (offsetY + dragAmount.y).coerceIn(-maxOffset, maxOffset)
-                        }
+                    .padding(top = 50.dp, start = 10.dp)
+                    .background(Color.Green)
+                    .onGloballyPositioned { position ->
+                        childSize = position.size
+                        childCoords = position
+                            .positionInRoot()
+                            .toIntOffset()
                     }
-            )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .offset { offset }
+                        .pointerInput(Unit) {
+                            detectDragGestures { change, dragAmount ->
+                                val newOffset = offset + dragAmount.toIntOffset()
+
+                                val newPosition = childCoords + newOffset
+
+                                if (
+                                    (newPosition.x > parentCoords.x) &&
+                                    (newPosition.x + childSize.width < parentCoords.x + parentSize.width) &&
+                                    (newPosition.y > parentCoords.y) &&
+                                    (newPosition.y + childSize.height < parentCoords.y + parentSize.height)
+                                ) {
+                                    offset = newOffset
+                                }
+                            }
+                        }
+                        .width(45.dp)
+                        .height(60.dp)
+                        .background(Color.Red)
+                )
+            }
         }
     }
+
+}
+
+fun Offset.toIntOffset(): IntOffset {
+    return IntOffset(x = this.x.roundToInt(), y = this.y.roundToInt())
 }
